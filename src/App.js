054477f4160d29f42239d56ab61bce80ee35464a
AlexDDK <dhrsmp@gmail.com>
2022-12-15 11:53:02 +0300

    init: start test task

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import chanelData from './24h.json'
+
+function App() {
+  console.log(chanelData);
+  return (
+    <div className="App">
+<h1>форма выбора каналов</h1>
+<p>Отметьте желательные каналы</p>
+
+    </div>
+  );
+}
+
+export default App;
